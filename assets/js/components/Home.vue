0977c67e91e8fd6aa0640be984c06cd49975d52c
Gabe Cook <gabe565@users.noreply.github.com>
2017-11-18 15:52:28 -0600

    Limit config numbers to the current players

--- assets/js/components/Home.vue
+++ assets/js/components/Home.vue
@@ -34,17 +34,20 @@
                                     <div class="card-text" v-if="valid" key="invalid">
                                         Here is the configuration for a {{ numPlayers }} player game:
                                         <ul class="list-unstyled">
-                                            <li v-for="(e, key) in currentConfig">
+                                            <li v-for="(e, key) in config">
                                                 <div class="col-10 mx-auto">
                                                     <div class="input-group py-1">
-                                                        <input type="number" v-model="currentConfig[key]" class="form-control"></input>
-                                                        <span class="input-group-addon" style="min-width: 120px;">
+                                                        <input type="number" v-model.number="config[key]" class="form-control" min="1" :max="maxs[key]" autocomplete="off"></input>
+                                                        <span class="input-group-addon" style="min-width: 130px;">
                                                             {{ key }}<span v-if="e > 1">s</span>
                                                         </span>
                                                     </div>
                                                 </div>
                                             </li>
                                         </ul>
+                                        <div class="text-center">
+                                            <button type="button" class="btn btn-primary" v-on:click="resetConfig">Reset</button>
+                                        </div>
                                     </div>
                                     <div class="card-text" v-else key="valid">
                                         This game requires at least {{ constraints.min }} players!
@@ -66,14 +69,12 @@
 import '../svg/plus'
 import '../svg/minus'
 
+import { defaultConfigs } from '../rules/configs'
+
 export default {
     data: function() {
         return {
             nextID: 5,
-            constraints: {
-                "min": 4,
-                "max": 8
-            },
             inputs: [
                 { id: 0, val: '' },
                 { id: 1, val: '' },
@@ -81,8 +82,7 @@ export default {
                 { id: 3, val: '' },
                 { id: 4, val: '' }
             ],
-            config: require('../json/configs.json'),
-            width: 90
+            configs: {}
         }
     },
     computed: {
@@ -102,10 +102,33 @@ export default {
             return this.numInputs < this.constraints.max
         },
         canRemove: function() {
-            return this.numInputs > this.constraints.min
+            return this.constraints.min < this.numInputs
         },
-        currentConfig: function() {
-            return this.config[this.numPlayers]
+        config: {
+            get: function() {
+                return this.configs[this.numPlayers]
+            },
+            set: function(newValue) {
+                this.configs[this.numPlayers] = newValue
+            }
+        },
+        constraints: function() {
+            var keys = _.keys(this.configs)
+            return {
+                'min': _.min(keys),
+                'max': _.max(keys)
+            }
+        },
+        maxs: function() {
+            var result = {}
+            var total = 0
+            _.forEach(this.config, function(value, key) {
+                total += value
+            })
+            if (total >= this.numPlayers)
+                return this.config
+            else
+                return _.mapValues(this.config, function(value) { return value + 1 })
         }
     },
     methods: {
@@ -116,11 +139,20 @@ export default {
             this.inputs.splice(i, 1)
         },
         submit: function() {
-            this.$router.push({ name: 'roles', params: { players: this.players, config: this.config[this.numPlayers] }})
+            this.$router.push({ name: 'roles', params: { players: this.players, config: this.config }})
         },
         id: function() {
             return this.nextID++
+        },
+        resetConfigs: function() {
+            this.configs = _.cloneDeep(defaultConfigs)
+        },
+        resetConfig: function() {
+            this.config = _.clone(defaultConfigs[this.numPlayers])
         }
     },
+    created: function() {
+        this.resetConfigs()
+    }
 }
 </script>
---
+++ assets/js/rules/configs.js
@@ -0,0 +1,27 @@
+export const defaultConfigs = {
+    "4": {
+        "Commander": 1,
+        "Agent": 2,
+        "Patrol": 1
+    },
+    "5": {
+        "Commander": 1,
+        "Agent": 2,
+        "Patrol": 2
+    },
+    "6": {
+        "Commander": 1,
+        "Agent": 3,
+        "Patrol": 2
+    },
+    "7": {
+        "Commander": 1,
+        "Agent": 4,
+        "Patrol": 2
+    },
+    "8": {
+        "Commander": 2,
+        "Agent": 4,
+        "Patrol": 2
+    }
+}
